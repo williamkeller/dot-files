e224d90b913d8c123941f22a4c8ef4a44989e9cc
William Keller <william@arda.net>
2025-09-28 13:34:44 -0500

    Rearranging options

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,12 +1,11 @@
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
+vim.opt.smartindent = true
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
-
-vim.opt.smartindent = true
 
 vim.opt.swapfile = false
 vim.opt.backup = false
