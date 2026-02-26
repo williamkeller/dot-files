56ecb5268baee0239392aa4bfe8ea39d78e17615
Will Keller <will.keller@prizepicks.com>
2026-02-26 16:29:47 -0600

    Tweaks

--- nvim/lua/plugins/completions.lua
+++ nvim/lua/plugins/completions.lua
@@ -15,8 +15,10 @@ return {
       local luasnip = require("luasnip")
 
       cmp.setup({
+        preselect = cmp.PreselectMode.None,
+
         completion = {
-          completeopt = "menu,menuone,noinsert"
+          completeopt = "menu,menuone,noselect"
         },
         snippet = {
           expand = function(args)
@@ -32,7 +34,7 @@ return {
           ['<C-f>'] = cmp.mapping.scroll_docs(4),
           ['<C-Space>'] = cmp.mapping.complete(),
           ['<C-e>'] = cmp.mapping.abort(),
-          ['<CR>'] = cmp.mapping.confirm({ select = true }),
+          ['<CR>'] = cmp.mapping.confirm({ select = false }),
           ['<Tab>'] = cmp.mapping(function(fallback)
             if luasnip.expand_or_jumpable() then
               luasnip.expand_or_jump()
