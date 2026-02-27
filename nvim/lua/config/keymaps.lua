67447553b404b98783f5a3102517e4064cde73c5
William Keller <william@arda.net>
2026-02-26 18:19:33 -0600

    Fixed keymaps

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -3,16 +3,10 @@
 
 vim.keymap.set("n", "<leader>d", vim.cmd.Ex)
 
--- Making switching between panes easier
-vim.keymap.set("n", "<C-h>", "<C-w>h")
-vim.keymap.set("n", "<C-l>", "<C-w>l")
-vim.keymap.set("n", "<C-j>", "<C-w>j")
-vim.keymap.set("n", "<C-k>", "<C-w>k")
-
 -- Make using the system clipboard a little easier
-vim.keymap.set("n", ",yy", '"+yy')
-vim.keymap.set({"n", "v"}, ",y", '"+y')
-vim.keymap.set("n", ",p", '"+p')
+vim.keymap.set("n", "YY", '"+yy')
+vim.keymap.set({"n", "v"}, "Y", '"+y')
+vim.keymap.set("n", "P", '"+p')
 
 -- Disable the stupid F1 key, that I seem to highlight
 -- every time I push ESC
