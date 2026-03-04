7f718f6b9ade2954592f1337203bc7431e91b692
Will Keller <will.keller@prizepicks.com>
2026-03-04 12:47:14 -0600

    Tweaks for sidekick mode

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -8,10 +8,11 @@ vim.keymap.set("n", "YY", '"+yy')
 vim.keymap.set({"n", "v"}, "Y", '"+y')
 vim.keymap.set("n", "P", '"+p')
 
+-- Exit terminal mode with C-k
+vim.keymap.set("t", "<Esc>", "<C-\\><C-n>", { noremap = true, silent = true })
+
 -- Disable the stupid F1 key, that I seem to highlight
 -- every time I push ESC
 vim.keymap.set({"n", "v", "i", "s"}, "<F1>", "<nop>", { noremap = true, silent = true })
-
-
 
 
